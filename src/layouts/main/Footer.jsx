fff4f7a2f81d99698c00a4ef5c31db97ae6e2904
limsongeun94 <cocorosaya@naver.com>
2022-12-15 15:24:55 +0900

    indexpage css

--- src/layouts/main/Footer.jsx
+++ src/layouts/main/Footer.jsx
@@ -1,5 +1,16 @@
 const Footer = (props) => {
-  return <footer>푸터</footer>;
+  return (
+    <footer className="footer-wrap">
+      <div>
+        <span>이용약관</span>
+        <span> | </span>
+        <span>개인정보처리방침</span>
+        <span> | </span>
+        <span>고객센터</span>
+      </div>
+      <div>Copyright ysk All rights reserved</div>
+    </footer>
+  );
 };
 
 export default Footer;
